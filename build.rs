70dcb35ac1301db967d3688f096fea0367d071f8
Ryan <ryan@ciehanski.com>
2019-09-04 23:15:27 -0500

    refactor, add travis building

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
 use std::process::Command;
 use std::ops::Add;
 
-#[cfg(any(target_os = "linux", target_os = "macos"))]
 fn main() {
     // macOS deps
     // brew install gettext automake autoconf libtool
@@ -14,88 +13,32 @@ fn main() {
         .to_string()
         + "/vendor";
 
+    if cfg!(linux) || cfg!(macos) || cfg!(freebsd) {
+        build_unix(&pwd);
+    } else if cfg!(windows) {
+        build_windows(&pwd);
+    } else {
+        panic!("incompatible operating system");
+    }
+
     // OpenSSL building and linking
-    Command::new("sh")
-        .current_dir(&pwd.clone().add("/openssl"))
-        .args(&[
-            "./config --prefix=$PWD/dist no-shared no-dso no-zlib",
-            "make depend",
-            "make",
-            "make install"])
-        .status()
-        .expect("Error building OpenSSL");
     println!("cargo:rustc-link-search=native=vendor/openssl/dist/lib");
     println!("cargo:rustc-link-lib=static=crypto");
     println!("cargo:rustc-link-lib=static=ssl");
 
     // Libevent building and linking
-    Command::new("sh")
-        .current_dir(&pwd.clone().add("/libevent"))
-        .args(&[
-            "./autogen.sh",
-            "./configure --prefix=$PWD/dist --disable-shared --enable-static --with-pic",
-            "make",
-            "make install"])
-        .status()
-        .expect("Error building Libevent");
     println!("cargo:rustc-link-search=native=vendor/libevent/dist/lib");
     println!("cargo:rustc-link-lib=static=event");
 
     // zlib building and linking
-    Command::new("sh")
-        .current_dir(&pwd.clone().add("/zlib"))
-        .args(&[
-            "./configure --prefix=$PWD/dist",
-            "make",
-            "make install"])
-        .status()
-        .expect("Error building zlib");
     println!("cargo:rustc-link-search=native=vendor/zlib/dist/lib");
     println!("cargo:rustc-link-lib=static=z");
 
     // xz building and linking
-    Command::new("sh")
-        .current_dir(&pwd.clone().add("/xz"))
-        .args(&[
-            "./autogen.sh",
-            "./configure --prefix=$PWD/dist \
-                --disable-shared \
-                --enable-static \
-                --disable-doc \
-                --disable-scripts \
-                --disable-xz \
-                --disable-xzdec \
-                --disable-lzmadec \
-                --disable-lzmainfo \
-                --disable-lzma-links",
-            "make",
-            "make install"])
-        .status()
-        .expect("Error building xz");
     println!("cargo:rustc-link-search=native=vendor/xz/dist/lib");
     println!("cargo:rustc-link-lib=static=lzma");
 
     // Tor building and linking
-    Command::new("sh")
-        .current_dir(&pwd.clone().add("/tor"))
-        .args(&[
-            "./autogen.sh",
-            "LIBS=-lcrypt32 ./configure --prefix=$PWD/dist \
-                --disable-gcc-hardening \
-                --enable-static-tor \
-                --enable-static-libevent \
-                --with-libevent-dir=$PWD/../libevent/dist \
-                --enable-static-openssl \
-                --with-openssl-dir=$PWD/../openssl/dist \
-                --enable-static-zlib \
-                --with-zlib-dir=$PWD/../openssl/dist \
-                --disable-system-torrc \
-                --disable-asciidoc",
-            "ln -s $PWD/../zlib/dist/lib/libz.a $PWD/../openssl/dist/lib/libz.a",
-            "make",
-            "make install"])
-        .status()
-        .expect("Error building Tor");
     println!("cargo:rustc-link-search=native=vendor/tor/src/ext/ed25519/ref10");
     println!("cargo:rustc-link-lib=static=ed25519_ref10");
 
@@ -122,3 +65,120 @@ fn main() {
     println!("cargo:rustc-link-lib=crypt32");
     println!("cargo:rustc-link-lib=gdi32");
 }
+
+fn build_unix(pwd: &String) {
+    // OpenSSL building
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/openssl"))
+        .args(&[
+            "./config",
+            "--prefix=$PWD/dist",
+            "no-shared",
+            "no-dso",
+            "no-zlib"])
+        .status()
+        .expect("Error building OpenSSL");
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/openssl"))
+        .arg("make depend")
+        .status()
+        .expect("Error building OpenSSL");
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/openssl"))
+        .arg("make")
+        .status()
+        .expect("Error building OpenSSL");
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/openssl"))
+        .arg("make install")
+        .status()
+        .expect("Error building OpenSSL");
+
+    // Libevent building
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/libevent"))
+        .arg("./autogen.sh")
+        .status()
+        .expect("Error building Libevent");
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/libevent"))
+        .args(&[
+            "./configure",
+            "--prefix=$PWD/dist",
+            "--disable-shared",
+            "--enable-static",
+            "--with-pic"])
+        .status()
+        .expect("Error building Libevent");
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/libevent"))
+        .arg("make")
+        .status()
+        .expect("Error building Libevent");
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/libevent"))
+        .arg("make install")
+        .status()
+        .expect("Error building Libevent");
+
+    // zlib building
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/zlib"))
+        .args(&["./configure", "--prefix=$PWD/dist"])
+        .status()
+        .expect("Error building zlib");
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/zlib"))
+        .arg("make")
+        .status()
+        .expect("Error building zlib");
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/zlib"))
+        .arg("make install")
+        .status()
+        .expect("Error building zlib");
+
+    // xz building
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/xz"))
+        .args(&[
+            "./autogen.sh",
+            "./configure --prefix=$PWD/dist \
+                --disable-shared \
+                --enable-static \
+                --disable-doc \
+                --disable-scripts \
+                --disable-xz \
+                --disable-xzdec \
+                --disable-lzmadec \
+                --disable-lzmainfo \
+                --disable-lzma-links",
+            "make",
+            "make install"])
+        .status()
+        .expect("Error building xz");
+
+    // Tor building and linking
+    Command::new("sh")
+        .current_dir(pwd.clone().add("/tor"))
+        .args(&[
+            "./autogen.sh",
+            "LIBS=-lcrypt32 ./configure --prefix=$PWD/dist \
+                --disable-gcc-hardening \
+                --enable-static-tor \
+                --enable-static-libevent \
+                --with-libevent-dir=$PWD/../libevent/dist \
+                --enable-static-openssl \
+                --with-openssl-dir=$PWD/../openssl/dist \
+                --enable-static-zlib \
+                --with-zlib-dir=$PWD/../openssl/dist \
+                --disable-system-torrc \
+                --disable-asciidoc",
+            "ln -s $PWD/../zlib/dist/lib/libz.a $PWD/../openssl/dist/lib/libz.a",
+            "make",
+            "make install"])
+        .status()
+        .expect("Error building Tor");
+}
+
+fn build_windows(pwd: &String) { /* TODO */ }
